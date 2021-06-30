99ea2057283c2d2322e1c2c0947dd8abdc8d8b59
Noemi Puskas <amifontos@gmail.com>
2021-06-30 02:07:47 +0200

    Tests: Added tests for the end and the beginning of workday

--- CalculateDueDate/calculateDueDate.test.js
+++ CalculateDueDate/calculateDueDate.test.js
@@ -2,6 +2,18 @@
 
 const dueDate = require('./calculateDueDate');
 
-test('Submitted in the morning, one hour turnaround', () => {
+test('Submitted in the morning, 1 hour turnaround', () => {
   expect(dueDate('1995-12-18T09:24:00.000Z', 1)).toBe('1995-12-18T10:24:00.000Z');
+});
+
+test('Submitted exactly at the beginning of the workday, 1 hour turnaround', () => {
+  expect(dueDate('1995-12-18T09:00:00.000Z', 1)).toBe('1995-12-18T10:00:00.000Z');
+});
+
+test('Ends exactly at the end of the workday, we expect no workday switches', () => {
+  expect(dueDate('1995-12-18T16:00:00.000Z', 1)).toBe('1995-12-18T17:00:00.000Z');
+});
+
+test('Submitted exactly at the beginning of the workday, 8 hours turnaround, we expect no workday switches', () => {
+  expect(dueDate('1995-12-18T09:00:00.000Z', 8)).toBe('1995-12-18T17:00:00.000Z');
 });
