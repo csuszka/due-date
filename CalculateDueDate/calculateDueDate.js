4b10efb5169fe4c0ea2bb07b7ed689ed5972b62f
Noemi Puskas <amifontos@gmail.com>
2021-06-30 21:29:33 +0200

    Test cases + base for calculating not same day completions (not working)

--- CalculateDueDate/calculateDueDate.js
+++ CalculateDueDate/calculateDueDate.js
@@ -5,11 +5,29 @@ const calculateDueDate = (submitDate, turnaroundTime) => {
   const turnaroundHours = Math.floor(turnaroundTime);
   const turnaroundMinutes = Math.floor(60 * (turnaroundTime % 1));
   const turnaroundSeconds = Math.floor(3600 * (turnaroundTime % 1 - turnaroundMinutes / 60));
+  const sameDay = ((dueDate.getHours() + turnaroundTime) < 17) ||
+    (((dueDate.getHours() + turnaroundHours) === 17) &&
+      ((dueDate.getMinutes() + turnaroundMinutes) === 0) &&
+      ((dueDate.getSeconds() + turnaroundSeconds) === 0));
 
-  dueDate.setHours(dueDate.getHours() + turnaroundHours);
-  dueDate.setMinutes(dueDate.getMinutes() + turnaroundMinutes);
-  dueDate.setSeconds(dueDate.getSeconds() + turnaroundSeconds);
+  if (sameDay) {
 
+    dueDate.setHours(dueDate.getHours() + turnaroundHours);
+    dueDate.setMinutes(dueDate.getMinutes() + turnaroundMinutes);
+    dueDate.setSeconds(dueDate.getSeconds() + turnaroundSeconds);
+
+  } else {
+    const hour = 3600000;
+    let sameDayEnd = new Date(submitDate);
+    sameDayEnd.setTime(17 * hour)
+
+    const remainingTurnaroundTime = turnaroundTime - (sameDayEnd - submitDate);
+
+    dueDate.setDate(dueDate.getDate() + Math.floor(remainingTurnaroundTime / 8))
+    dueDate.setTime((9 + (remainingTurnaroundTime % 8)) * hour)
+
+
+  }
   return dueDate.toISOString();
 }
 
--- CalculateDueDate/calculateDueDate.test.js
+++ CalculateDueDate/calculateDueDate.test.js
@@ -2,6 +2,8 @@
 
 const dueDate = require('./calculateDueDate');
 
+//Same day
+
 test('Submitted in the morning, 1 hour turnaround', () => {
   expect(dueDate('1995-12-18T09:24:00.000Z', 1)).toBe('1995-12-18T10:24:00.000Z');
 });
@@ -16,4 +18,28 @@ test('Ends exactly at the end of the workday, we expect no workday switches', ()
 
 test('Submitted exactly at the beginning of the workday, 8 hours turnaround, we expect no workday switches', () => {
   expect(dueDate('1995-12-18T09:00:00.000Z', 8)).toBe('1995-12-18T17:00:00.000Z');
+});
+
+//Same week
+
+test('Submitted in the morning, 16 hour turnaround', () => {
+  expect(dueDate('2021-06-28T10:00:00.000Z', 16)).toBe('2021-06-30T10:00:00.000Z');
+});
+
+//Different week
+
+test('Submitted in the morning, 16 hour turnaround', () => {
+  expect(dueDate('2021-06-25T10:00:00.000Z', 16)).toBe('2021-06-29T10:00:00.000Z');
+});
+
+//Different month
+
+test('Submitted in the morning, 16 hour turnaround', () => {
+  expect(dueDate('2021-06-30T10:00:00.000Z', 16)).toBe('2021-07-02T10:00:00.000Z');
+});
+
+//Different year
+
+test('Submitted in the morning, 16 hour turnaround', () => {
+  expect(dueDate('2020-12-31T10:00:00.000Z', 16)).toBe('2021-01-04T10:00:00.000Z');
 });
